fb7ad55441373231e5a0344eb53c8816d61fe470
MathcaTi <tehmatcha28@gmail.com>
2023-07-04 13:27:36 +0800

    setup colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -9,10 +10,24 @@ module.exports = {
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: {
+        'nondenoic-main': '#3b82f6',
+        'nondenoic-secondary': '#60a5fa',
+        'light-base': '#f1f5f9',
+        'light-main': '#ffffff',
+        'light-secondary': '#f8fafc',
+        'light-accent': '#e2e8f0',
+        'light-text': '#64748b',
+        'dark-base': '#0f172a',
+        'dark-main': '#152239',
+        'dark-secondary': '#18273F',
+        'dark-accent': '#1b3152',
+        'dark-text': '#f1f5f9',
+        'code-editor': '#121d2f',
       },
     },
   },
   plugins: [],
-}
+};
