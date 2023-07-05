13aab931cd1d88edc97306dbe05bbcb482a85290
MathcaTi <tehmatcha28@gmail.com>
2023-07-05 13:13:50 +0800

    add new utils time and new interface

--- src/libs/utils.ts
+++ src/libs/utils.ts
@@ -1,3 +1,15 @@
+import { DateTimeFormatOptions } from './interfaces';
+
 export function cn(...cns: Array<string>) {
   return cns.join(' ');
+}
+
+export function time(val: any) {
+  let options: DateTimeFormatOptions = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  };
+
+  return new Intl.DateTimeFormat('id-ID', options).format(new Date(val));
 }
