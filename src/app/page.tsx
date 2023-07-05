d15a23ca4530853dfb6689126d5c2867745e06fa
MathcaTi <tehmatcha28@gmail.com>
2023-07-05 17:34:46 +0800

    update main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import PostsList from '@/components/common/postsList';
 import Hero from '@/components/layout/hero';
 
 export default function Home() {
   return (
     <main className='relative z-10 mx-auto min-h-screen w-full'>
       <Hero />
+      <PostsList />
     </main>
   );
 }
