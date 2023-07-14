1f4fa4227571f915a2a3ef5d62c724315c62b44a
MathcaTi <tehmatcha28@gmail.com>
2023-07-14 16:31:21 +0800

    add new component portfolio & update hero section

--- src/components/layout/hero.tsx
+++ src/components/layout/hero.tsx
@@ -5,7 +5,7 @@ const jetBrains = JetBrains_Mono({ subsets: ['latin'] });
 export default function Hero() {
   return (
     <div className='h-screen w-full snap-start bg-dark-base'>
-      <section className='mx-auto grid h-full w-full max-w-7xl grid-rows-3 flex-col items-center p-4'>
+      <section className='mx-auto grid h-full w-full max-w-7xl grid-rows-3 items-center p-8'>
         <h1
           className={`${jetBrains.className} row-start-2 justify-self-center text-5xl font-bold text-dark-headings sm:text-6xl xl:text-7xl`}
         >
---
+++ src/components/layout/portfolio.tsx
@@ -0,0 +1,32 @@
+import { JetBrains_Mono } from 'next/font/google';
+
+const jetBrains = JetBrains_Mono({ subsets: ['latin'] });
+
+export default function Portfolio() {
+  return (
+    <div className='h-screen w-full snap-start bg-light-base'>
+      <section className='mx-auto grid h-full w-full max-w-7xl items-center justify-center p-8 md:grid-cols-2'>
+        <div className='w-full space-y-4 text-center md:text-left'>
+          <h2 className={`${jetBrains.className} text-xl font-bold text-light-headings md:text-2xl xl:text-3xl`}>
+            {"Hello, it's me"}
+          </h2>
+          <h2 className={`${jetBrains.className} text-5xl font-semibold text-light-headings md:text-6xl xl:text-7xl`}>
+            AdiIfai<span className='text-nondenoic-main'>.</span>
+          </h2>
+          <p className='text-justify text-light-text'>
+            Saya adalah seorang front-end developer dengan kecintaan terhadap web design dan web styling, saya mampu
+            untuk mengimplementasikan desain dan membangun antarmuka web yang konsisten dan responsif. Saya senang
+            berkreasi dalam menggabungkan komponen visual yang menarik dengan interaksi yang halus dan menciptakan
+            tampilan web yang menarik, modern, dan fungsional untuk meningkatkan kepuasan pengguna
+            <span className='text-nondenoic-main'>.</span>
+          </p>
+          <p
+            className={`${jetBrains.className} text-left font-semibold text-light-headings underline decoration-nondenoic-main underline-offset-8`}
+          >
+            Scroll for more
+          </p>
+        </div>
+      </section>
+    </div>
+  );
+}
