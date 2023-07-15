5b17323c793d93d177ee19e55a0c4987d42adfa0
MathcaTi <tehmatcha28@gmail.com>
2023-07-15 20:53:38 +0800

    wip layout gallery

--- src/components/layout/gallery.tsx
+++ src/components/layout/gallery.tsx
@@ -4,38 +4,21 @@ const jetBrains = JetBrains_Mono({ subsets: ['latin'] });
 
 export default function Gallery() {
   return (
-    <div className='relative min-h-screen w-full snap-start bg-dark-base py-8 lg:h-screen'>
-      <section className='mx-auto h-full w-full max-w-7xl p-8'>
-        <div className='grid h-full gap-4 sm:grid-cols-2 lg:grid-cols-12 lg:grid-rows-12'>
-          <div className='w-full space-y-4 text-center sm:text-left lg:col-span-4 lg:col-start-5 lg:row-span-4 lg:row-start-5'>
-            <h2 className={`${jetBrains.className} text-xl font-bold text-dark-headings lg:text-3xl`}>{"Let's"}</h2>
-            <h2 className={`${jetBrains.className} text-5xl font-semibold text-dark-headings lg:text-7xl`}>
-              Explore Gallery.
-            </h2>
+    <div className='relative h-screen w-full snap-start bg-dark-base py-8'>
+      <section className='mx-auto grid h-full w-full max-w-7xl items-center p-8 lg:items-stretch'>
+        <div>
+          <div className='mb-4 text-3xl text-dark-text sm:text-4xl lg:col-span-3 lg:col-start-7 lg:row-span-3 lg:row-start-4 lg:hidden lg:text-5xl'>
+            <h2 className={`${jetBrains.className} font-bold text-dark-headings`}>{"Let's"}</h2>
+            <h2 className={`${jetBrains.className} font-bold text-dark-headings`}>Explore Gallery.</h2>
           </div>
-          <div className='clipped-container flex h-52 items-center justify-center bg-dark-secondary lg:col-span-3 lg:col-start-2 lg:row-span-3 lg:row-start-2 lg:h-auto'>
-            <div className={`${jetBrains.className} clipped font-semibold duration-300 ease-in-out`}>Comming Soon.</div>
-          </div>
-          <div className='clipped-container flex h-52 items-center justify-center bg-dark-secondary lg:col-span-3 lg:col-start-5 lg:row-span-3 lg:row-start-1 lg:h-auto'>
-            <div className={`${jetBrains.className} clipped font-semibold duration-300 ease-in-out`}>Comming Soon.</div>
-          </div>
-          <div className='clipped-container flex h-52 items-center justify-center bg-dark-secondary lg:col-span-3 lg:col-start-9 lg:row-span-3 lg:row-start-2 lg:h-auto'>
-            <div className={`${jetBrains.className} clipped font-semibold duration-300 ease-in-out`}>Comming Soon.</div>
-          </div>
-          <div className='clipped-container flex h-52 items-center justify-center bg-dark-secondary lg:col-span-3 lg:col-start-10 lg:row-span-3 lg:row-start-5 lg:h-auto'>
-            <div className={`${jetBrains.className} clipped font-semibold duration-300 ease-in-out`}>Comming Soon.</div>
-          </div>
-          <div className='clipped-container flex h-52 items-center justify-center bg-dark-secondary lg:col-span-3 lg:col-start-2 lg:row-span-3 lg:row-start-9 lg:h-auto'>
-            <div className={`${jetBrains.className} clipped font-semibold duration-300 ease-in-out`}>Comming Soon.</div>
-          </div>
-          <div className='clipped-container flex h-52 items-center justify-center bg-dark-secondary lg:col-span-3 lg:col-start-1 lg:row-span-3 lg:row-start-6 lg:h-auto'>
-            <div className={`${jetBrains.className} clipped font-semibold duration-300 ease-in-out`}>Comming Soon.</div>
-          </div>
-          <div className='clipped-container flex h-52 items-center justify-center bg-dark-secondary lg:col-span-3 lg:col-start-9 lg:row-span-3 lg:row-start-9 lg:h-auto'>
-            <div className={`${jetBrains.className} clipped font-semibold duration-300 ease-in-out`}>Comming Soon.</div>
-          </div>
-          <div className='clipped-container flex h-52 items-center justify-center bg-dark-secondary lg:col-span-3 lg:col-start-6 lg:row-span-3 lg:row-start-10 lg:h-auto'>
-            <div className={`${jetBrains.className} clipped font-semibold duration-300 ease-in-out`}>Comming Soon.</div>
+          <div className='flex h-full w-full snap-x snap-mandatory gap-1 overflow-auto lg:grid lg:snap-none lg:grid-cols-12 lg:grid-rows-12'>
+            <div className='hidden items-center justify-center gap-4 text-3xl text-dark-text sm:flex-col sm:gap-0 sm:text-4xl lg:col-span-3 lg:col-start-7 lg:row-span-3 lg:row-start-4 lg:flex lg:text-5xl'>
+              <h2 className={`${jetBrains.className} font-bold text-dark-headings`}>{"Let's"}</h2>
+              <h2 className={`${jetBrains.className} font-bold text-dark-headings`}>Explore Gallery.</h2>
+            </div>
+            <div className='mb-4 flex aspect-square h-full w-4/5 max-w-sm flex-none snap-center scroll-mx-4 bg-neutral-700 lg:col-start-8 lg:row-start-1 lg:mb-0 lg:aspect-auto lg:w-full lg:max-w-none'></div>
+            <div className='mb-4 flex aspect-square h-full w-4/5 max-w-sm flex-none snap-center scroll-mx-4 bg-neutral-700 lg:col-start-7 lg:row-start-1 lg:mb-0 lg:aspect-auto lg:w-full lg:max-w-none'></div>
+            <div className='mb-4 flex aspect-square h-full w-4/5 max-w-sm flex-none snap-center scroll-mx-4 bg-neutral-700 lg:col-start-6 lg:row-start-1 lg:mb-0 lg:aspect-auto lg:w-full lg:max-w-none'></div>
           </div>
         </div>
       </section>
