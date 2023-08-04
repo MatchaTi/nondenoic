5c3eb6f6dbf4df72e0da04ba375094278c951c69
MathcaTi <tehmatcha28@gmail.com>
2023-08-04 20:31:21 +0800

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import PostsList from '@/components/common/postsList';
-import Footer from '@/components/layout/footer';
 import Hero from '@/components/layout/hero';
 import Portfolio from '@/components/layout/portfolio';
 import RecentProjects from '@/components/layout/recentProjects';
@@ -7,15 +6,12 @@ import SkillsAndTools from '@/components/layout/skillsAndTools';
 
 export default function Home() {
   return (
-    <main className='no-scrollbar relative z-10 mx-auto h-screen w-full snap-y snap-mandatory overflow-y-auto overflow-x-hidden'>
-      <div>
-        <Hero />
-        <Portfolio />
-        <SkillsAndTools />
-        <RecentProjects />
-        <PostsList />
-        <Footer />
-      </div>
+    <main>
+      <Hero />
+      <Portfolio />
+      <SkillsAndTools />
+      <RecentProjects />
+      <PostsList />
     </main>
   );
 }
