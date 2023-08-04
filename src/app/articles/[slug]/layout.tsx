774f6dd3d4e5245193022ec9dec85266989b5596
MathcaTi <tehmatcha28@gmail.com>
2023-08-04 20:32:34 +0800

    update

--- src/app/articles/[slug]/layout.tsx
+++ src/app/articles/[slug]/layout.tsx
@@ -1,12 +1,10 @@
-import Footer from '@/components/layout/footer';
 import GridBg from '@/components/layout/gridBg';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <div>
-      <GridBg />
+      <GridBg full />
       {children}
-      <Footer />
     </div>
   );
 }
