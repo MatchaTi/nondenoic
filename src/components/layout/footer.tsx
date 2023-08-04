42909e802c524b074d48a53101890d70edc77357
MathcaTi <tehmatcha28@gmail.com>
2023-08-04 20:32:47 +0800

    update

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,13 +1,13 @@
 import Image from 'next/image';
-import NondenoicIcon from '../../../public/images/nondenoic-icon.svg';
+import NondenoicIcon from '../../../public/images/nondenoic-icon.png';
 
 export default function Footer() {
   return (
-    <footer className='bg-base-secondary relative -z-20 w-full snap-end border-t border-light-accent/5'>
-      <div className='mx-auto w-full max-w-7xl px-4'>
+    <footer className='relative -z-20 w-full border-t border-neutral-200'>
+      <div className='mx-auto w-full max-w-7xl px-8'>
         <div className='flex w-full flex-wrap items-center justify-between gap-4 py-6 sm:gap-0 lg:px-4'>
           <div className='h-10 w-10 rounded'>
-            <Image src={NondenoicIcon} alt='Nondenoic Icon' />
+            <Image src={NondenoicIcon} alt='Nondenoic Icon' className='rounded' />
           </div>
           <div className='opacity-80'>&copy; 2023 Nondenoic by AdiIfai</div>
         </div>
