98c3289414c0a0ad694cdd88ee692fca31838fb4
MathcaTi <tehmatcha28@gmail.com>
2023-08-04 20:32:38 +0800

    update

--- src/app/articles/[slug]/page.tsx
+++ src/app/articles/[slug]/page.tsx
@@ -17,8 +17,8 @@ export default function PostLayout({ params }: { params: { slug: string } }) {
   const MDXContent = useMDXComponent(post.body.code);
 
   return (
-    <article className='mt-20 w-full border-y border-light-accent/5 lg:mb-28'>
-      <section className='mx-auto w-full max-w-7xl p-4'>
+    <article className='mt-20 w-full border-y border-neutral-200 lg:mb-28'>
+      <section className='mx-auto w-full max-w-7xl p-4 px-8'>
         <div className='mb-10 w-full overflow-hidden'>
           <div className='aspect-video sm:px-4'>
             <Image src={`${post.imagePreview}`} alt={post.title} width={1920} height={1080} className='rounded-xl' />
@@ -26,18 +26,18 @@ export default function PostLayout({ params }: { params: { slug: string } }) {
         </div>
         <div className='w-full grid-cols-3 lg:grid'>
           <div className='order-1 mb-10 sm:px-4 lg:col-span-2'>
-            <h1 className='mb-2 text-lg font-bold text-dark-headings sm:text-3xl'>{post.title}</h1>
+            <h1 className='text-dark-headings mb-2 text-lg font-bold sm:text-3xl'>{post.title}</h1>
             <p className='mb-4'>{post.excerpt}</p>
           </div>
           <div className='custom-prose order-3 mb-10 w-full sm:px-4 lg:col-span-2 lg:mb-0'>
             <MDXContent components={mdxComponents} />
           </div>
           <div className='z-10 order-2 sm:px-4 lg:sticky lg:top-20 lg:px-6'>
-            <div className='mb-4 border-b border-light-accent/5 pb-4'>
-              <div className='text-lg font-semibold text-dark-headings'>{post.author}</div>
+            <div className='border-light-accent/5 mb-4 border-b pb-4'>
+              <div className='text-dark-headings text-lg font-semibold'>{post.author}</div>
               <time className='text-sm opacity-80'>Diposting {time(post.date)}</time>
             </div>
-            <h2 className='mb-4 text-xl font-semibold text-dark-headings'>Artikel Lainnya</h2>
+            <h2 className='text-dark-headings mb-4 text-xl font-semibold'>Artikel Lainnya</h2>
             <ul>
               {anotherPosts.length > 0 ? (
                 anotherPosts.map((post, i) => (
